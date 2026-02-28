432276641615d80e94ef1ac6739546dded79848c
snigdhagupta67 <Snigdha.25692@stu.upes.ac.in>
2026-02-28 12:42:35 +0530

    day28

---
+++ day28.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, value;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    
+    if(temp != NULL) {
+        temp->next = head;
+    }
+
+
+    if(head != NULL) {
+        temp = head;
+        do {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        } while(temp != head);
+    }
+
+    return 0;
+}
