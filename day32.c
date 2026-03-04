534ce64991f9f82eca9e8a140d7985d505fd2bf2
snigdhagupta67 <Snigdha.25692@stu.upes.ac.in>
2026-03-04 23:54:30 +0530

    day32

---
+++ day32.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    top++;
+    stack[top] = value;
+}
+
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    top--;
+}
+
+int main() {
+    int n, m, i, value;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &value);
+        push(value);
+    }
+
+    scanf("%d", &m);
+
+    for (i = 0; i < m; i++) {
+        pop();
+    }
+
+    for (i = top; i >= 0; i--) {
+        printf("%d ", stack[i]);
+    }
+
+    return 0;
+}
