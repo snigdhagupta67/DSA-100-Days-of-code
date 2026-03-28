7def294cb296f9c8a207a5303f7b7cb466a33693
snigdhagupta67 <Snigdha.25692@stu.upes.ac.in>
2026-03-28 23:49:51 +0530

    day 56

---
+++ day56.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+
+struct Node* newNode(int data) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+
+int isMirror(struct Node* t1, struct Node* t2) {
+    if (t1 == NULL && t2 == NULL)
+        return 1;
+
+    if (t1 == NULL || t2 == NULL)
+        return 0;
+
+    return (t1->data == t2->data) &&
+           isMirror(t1->left, t2->right) &&
+           isMirror(t1->right, t2->left);
+}
+
+int isSymmetric(struct Node* root) {
+    return isMirror(root, root);
+}
+
+int main() {
+   
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(2);
+    root->left->left = newNode(3);
+    root->left->right = newNode(4);
+    root->right->left = newNode(4);
+    root->right->right = newNode(3);
+
+    if (isSymmetric(root))
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+}
